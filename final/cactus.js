7153c2b4c75cdb1ddefa1e41bc98d887e3356fda
User <user@lab1280m.tumo.lab>
2022-09-13 18:36:19 +0400

    second commit

--- final/cactus.js
+++ final/cactus.js
@@ -1,7 +1,6 @@
-class Cactus{
+class Cactus extends LivingCreature {
     constructor(x, y) {
-     this.x = x;
-     this.y = y;
+        super(x, y)
      this.energy = 8;
      this.multiply = 0
      this.directions = [];
